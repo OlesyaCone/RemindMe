749948d6585ff2fc6c62c09ad64cebe6a9fe743a
OlesyaCone <o1961232@gmail.com>
2025-10-27 20:45:32 +0300

    host

--- d_client/vite.config.ts
+++ d_client/vite.config.ts
@@ -4,8 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', 
+  base: process.env.NODE_ENV === 'production' ? '/d_client/' : '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
   }
 })
